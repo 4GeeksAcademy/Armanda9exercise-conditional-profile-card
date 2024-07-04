3cc862dfb5138cd7adee8e9f88432fecff6ce40f
ARMANDA2022 <104578464+ARMANDA2022@users.noreply.github.com>
2024-07-04 21:15:25 +0000

    terminado

--- src/js/app.js
+++ src/js/app.js
@@ -4,10 +4,10 @@ import "../style/index.css";
  *  EDIT ONLY INSIDE THIS RENDER FUNCTION
  *  This function is called every time the user changes types or changes any input
  * 
-    {
+   variables = {
         includeCover: true, // if includeCover is true the algorithm should show the cover image
         background: "https://images.unsplash.com/photo-1511974035430-5de47d3b95da", // this is the image's url that will be used as a background for the profile cover
-        avatarURL: "https://randomuser.me/api/portraits/women/42.jpg", // this is the url for the profile avatar
+        avatarURL: "https://i.ibb.co/M9Y9Whf/zeny-foto.jpg", // this is the url for the profile avatar
         socialMediaPosition: "right", // social media bar position (left or right)
         
         twitter: null, // social media usernames
@@ -21,7 +21,12 @@ import "../style/index.css";
         country: null,
         city: null
     }
+
+ 
+    
  */
+//creamos la función y le damos el parametro variables igual a objetos
+
 function render(variables = {}) {
   console.log("These are the current variables: ", variables); // print on the console
   // here we ask the logical questions to make decisions on how to build the html
@@ -30,17 +35,28 @@ function render(variables = {}) {
   if (variables.includeCover == false) cover = "<div class='cover'></div>";
 
   // reset the website body with the new html output
+
+  //Aqui llamamos atravez de querySelector llamo al id="widget-content" del div del HTML.
+  //Aqui usamos la funciòn $ para llamar elementos del DOM "a los imput creados en html" y concatena la funcion render con el varametro variables
+
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
-          <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
-          <ul class="position-right">
-            <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/school/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>
+           <img src="${variables.avatarURL}" class="photo" />
+          <h1>${variables.name}, ${
+    variables.lastName
+  }</h1>                        
+          <h3>${variables.role}</h3>
+          <h3>${variables.city}, ${variables.country}</h3>          
+          <ul class= ${variables.SMPosition}>
+            <li><a href="${"https://twitter.com/" +
+              variables.twitter}"><i class="fab fa-twitter"></i></a></li>
+            <li><a href="${"https://github.com/" +
+              variables.github}"><i class="fab fa-github"></i></a></li>
+            <li><a href="https://linkedin.com/in/${
+              variables.linkedin
+            }"><i class="fab fa-linkedin"></i></a></li>
+            <li><a href="${"https://instagram.com/" +
+              variables.instagram}"><i class="fab fa-instagram"></i></a></li>
           </ul>
         </div>
     `;
@@ -49,43 +65,45 @@ function render(variables = {}) {
 /**
  * Don't change any of the lines below, here is where we do the logic for the dropdowns
  */
-window.onload = function() {
-  window.variables = {
-    // if includeCover is true the algorithm should show the cover image
-    includeCover: true,
-    // this is the image's url that will be used as a background for the profile cover
-    background: "https://images.unsplash.com/photo-1511974035430-5de47d3b95da",
-    // this is the url for the profile avatar
-    avatarURL: "https://randomuser.me/api/portraits/women/42.jpg",
-    // social media bar position (left or right)
-    socialMediaPosition: "position-left",
-    // social media usernames
-    twitter: null,
-    github: null,
-    linkedin: null,
-    instagram: null,
-    name: null,
-    lastName: null,
-    role: null,
-    country: null,
-    city: null
-  };
-  render(window.variables); // render the card for the first time
 
-  document.querySelectorAll(".picker").forEach(function(elm) {
-    elm.addEventListener("change", function(e) {
-      // <- add a listener to every input
-      const attribute = e.target.getAttribute("for"); // when any input changes, collect the value
-      let values = {};
-      values[attribute] =
-        this.value == "" || this.value == "null"
-          ? null
-          : this.value == "true"
-          ? true
-          : this.value == "false"
-          ? false
-          : this.value;
-      render(Object.assign(window.variables, values)); // render again the card with new values
-    });
-  });
+console.log("mis variables vacias", window.variables);
+
+window.variables = {
+  // if includeCover is true the algorithm should show the cover image
+  includeCover: true,
+  // this is the image's url that will be used as a background for the profile cover
+  background:
+    "http://www.viajejet.com/wp-content/viajes/Lago-Moraine-Parque-Nacional-Banff-Alberta-Canada.jpg",
+  // this is the url for the profile avatar
+  avatarURL: "https://i.ibb.co/M9Y9Whf/zeny-foto.jpg",
+  // social media bar position (left or right)
+  SMPosition: "position-left",
+  // social media usernames
+  twitter: "twitter",
+  github: null,
+  linkedin: null,
+  instagram: null,
+  name: "name",
+  lastName: "lastName",
+  role: "role",
+  country: "country",
+  city: "city"
 };
+render(window.variables); // render the card for the first time
+
+document.querySelectorAll(".picker").forEach(function(elm) {
+  elm.addEventListener("change", function(e) {
+    // <- add a listener to every input
+    const attribute = e.target.getAttribute("for"); // when any input changes, collect the value
+    let values = {};
+    values[attribute] =
+      this.value == "" || this.value == "null"
+        ? null
+        : this.value == "true"
+        ? true
+        : this.value == "false"
+        ? false
+        : this.value;
+    render(Object.assign(window.variables, values)); // render again the card with new values
+  });
+});
